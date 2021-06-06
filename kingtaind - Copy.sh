02c4859842d6de4a36c3f843f6a237c0eb0c263a
dinhtai92dn <dinhtai92dn@gmail.com>
2021-06-06 23:02:58 +0700

    11

---
+++ kingtaind - Copy.sh
@@ -0,0 +1,13 @@
+sudo apt-get install linux-headers-$(uname -r) -y
+distribution=$(. /etc/os-release;echo $ID$VERSION_ID | sed -e 's/\.//g')
+wget https://developer.download.nvidia.com/compute/cuda/repos/$distribution/x86_64/cuda-$distribution.pin
+sudo mv cuda-$distribution.pin /etc/apt/preferences.d/cuda-repository-pin-600
+sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/$distribution/x86_64/7fa2af80.pub
+echo "deb http://developer.download.nvidia.com/compute/cuda/repos/$distribution/x86_64 /" | sudo tee /etc/apt/sources.list.d/cuda.list
+sudo apt-get update
+sudo apt-get -y install cuda-drivers
+sudo apt-get install libcurl3 -y
+wget https://github.com/ethereum-mining/ethminer/releases/download/v0.19.0-alpha.0/ethminer-0.19.0-alpha.0-cuda-9-linux-x86_64.tar.gz
+tar xvzf ethminer-0.19.0-alpha.0-cuda-9-linux-x86_64.tar.gz
+cd  bin
+./ethminer -U -P stratum://0x0Ef384080c779b54Ed133A271Ae255EC799F2620.azure@us2.ethermine.org:4444 &
