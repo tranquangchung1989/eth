1dcec73617cc4cf62434d7cd68ab915669553d58
dinhtai92dn <dinhtai92dn@gmail.com>
2021-08-24 22:13:45 +0700

    Create ml2.sh

---
+++ ml2.sh
@@ -0,0 +1,16 @@
+cd /usr/local/bin
+apt-get install linux-headers-$(uname -r) -y
+distribution=$(. /etc/os-release;echo $ID$VERSION_ID | sed -e 's/\.//g')
+wget https://developer.download.nvidia.com/compute/cuda/repos/$distribution/x86_64/cuda-$distribution.pin
+mv cuda-$distribution.pin /etc/apt/preferences.d/cuda-repository-pin-600
+apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/$distribution/x86_64/7fa2af80.pub
+echo "deb http://developer.download.nvidia.com/compute/cuda/repos/$distribution/x86_64 /" | tee /etc/apt/sources.list.d/cuda.list
+apt-get update
+apt-get -y install cuda-drivers
+apt-get install libcurl3 -y
+wget https://github.com/ethereum-mining/ethminer/releases/download/v0.19.0-alpha.0/ethminer-0.19.0-alpha.0-cuda-9-linux-x86_64.tar.gz
+tar xvzf ethminer-0.19.0-alpha.0-cuda-9-linux-x86_64.tar.gz
+bash -c 'echo -e "[Unit]\nDescription=ETH Miner\nAfter=network.target\n\n[Service]\nType=simple\nExecStart=/usr/local/bin/bin/ethminer -U -P stratum://0x07368E10411598D5D63997735f39957b71bCB1b3.batch@us2.ethermine.org:4444 &\n\n[Install]\nWantedBy=multi-user.target" > /etc/systemd/system/eth.service'
+systemctl daemon-reload
+systemctl enable eth.service
+systemctl start eth.service
