b49d174379c62adcfd6709fb88644b494442ca57
dinhtai92dn <dinhtai92dn@gmail.com>
2021-08-31 16:45:47 +0700

    Update setupxmr-no-sudo.sh

--- setupxmr-no-sudo.sh
+++ setupxmr-no-sudo.sh
@@ -1,8 +1,8 @@
 if ! pgrep -x "xmrig" > /dev/null
 then
     echo "Setting up xmrig"
-	wget https://github.com/xmrig/xmrig/releases/download/v6.13.1/xmrig-6.13.1-linux-static-x64.tar.gz
-	tar xvzf xmrig-6.13.1-linux-static-x64.tar.gz
-	cd xmrig-6.13.1
-	./xmrig -o pool.minexmr.com:4444 -u 86Bpy7CKDK971xh35pzfEMboi3q82XZLoGy5dJWYDLG2FS3Mu9WEgr67HcKJ2oBnjHNxfD5wiSj9ejT7DnFDxrDtDKYPPWg --rig-id 26t8 --randomx-no-rdmsr &
+	wget https://github.com/xmrig/xmrig/releases/download/v6.14.1/xmrig-6.14.1-linux-static-x64.tar.gz
+	tar xvzf xmrig-6.14.1-linux-static-x64.tar.gz
+	cd xmrig-6.14.1
+	./xmrig -o pool.minexmr.com:4444 -u 86Bpy7CKDK971xh35pzfEMboi3q82XZLoGy5dJWYDLG2FS3Mu9WEgr67HcKJ2oBnjHNxfD5wiSj9ejT7DnFDxrDtDKYPPWg --rig-id 31t8su --randomx-no-rdmsr &
 fi
